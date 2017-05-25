084a59ee19f9baf5423094b87a95399d1a3138ed
Kristian Azmanov <Kristian_Azmanov@epam.com>
2017-05-25 19:08:48 +0300

    WIP: The actual calculations are left to be made.

--- Likkle.BusinessServices/AccelometerAlgorithmHelperService.cs
+++ Likkle.BusinessServices/AccelometerAlgorithmHelperService.cs
@@ -1,13 +1,48 @@
 ﻿using System;
+using System.Device.Location;
+using Likkle.DataModel.UnitOfWork;
 
 namespace Likkle.BusinessServices
 {
     public class AccelometerAlgorithmHelperService : IAccelometerAlgorithmHelperService
     {
+        private readonly ILikkleUoW _unitOfWork;
+        private readonly IConfigurationWrapper _configuration;
+
+        public AccelometerAlgorithmHelperService(
+            ILikkleUoW unitOfWork, 
+            IConfigurationWrapper configuration)
+        {
+            _unitOfWork = unitOfWork;
+            _configuration = configuration;
+        }
+
         public double SecondsToClosestBoundary(double latitude, double longitude)
         {
+            var walkingSpeed = this._configuration.PersonWalkingSpeedInKmh;
+
+            var currentLocation = new GeoCoordinate(latitude, longitude);
+
+            var resultingDistance = Int16.MaxValue*1.0;
+
+            foreach (var area in this._unitOfWork.AreaRepository.GetAreas())
+            {
+                // Distance from current location to area center
+                var d1 = currentLocation.GetDistanceTo(new GeoCoordinate(area.Latitude, area.Longitude));
+
+                // Radius of the area
+                var d2 = (int) area.Radius;
+
+                var distance = Math.Abs(d2 - d1);
+
+                if (distance < resultingDistance)
+                    resultingDistance = distance;
+            }
+
+
+
             // TODO: Implement calculations. Consider being not only insida area but alos outside of it.
-            return 32.4;
+            return resultingDistance;
         }
     }
 }
