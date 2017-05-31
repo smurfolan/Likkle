6828b9bea77d7700a03b46daab8ff7ada3f6cb16
Kristian Azmanov <Kristian_Azmanov@epam.com>
2017-05-31 15:24:19 +0300

    Fixed validation when updating user info

--- Likkle.BusinessServices/Validators/UpdatedUserInfoRequestDtoValidator.cs
+++ Likkle.BusinessServices/Validators/UpdatedUserInfoRequestDtoValidator.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using FluentValidation;
 using Likkle.BusinessEntities.Requests;
 using Likkle.BusinessServices.Utils;
@@ -9,25 +10,28 @@ namespace Likkle.BusinessServices.Validators
     {
         private readonly IPhoneValidationManager _phoneValidationManager;
         private readonly IUserService _userService;
+        private readonly Guid _userId;
 
         public UpdatedUserInfoRequestDtoValidator(
+            Guid id,
             IPhoneValidationManager phoneValidationManager, 
             IUserService userService)
         {
             _phoneValidationManager = phoneValidationManager;
             _userService = userService;
+            _userId = id;
 
             RuleFor(r => r.FirstName).NotNull();
             RuleFor(r => r.LastName).NotNull();
             RuleFor(r => r.Email).NotNull();
 
             RuleFor(r => r.PhoneNumber).Must(BeAValidPhoneNumber).WithMessage("Phone number provided is invalid");
-            RuleFor(r => r.Email).Must(BeUniqueEmail).WithMessage("User with the same email has been already added.");
+            RuleFor(r => r.Email).EmailAddress().Must(BeUniqueEmail).WithMessage("User with the same email has been already added.");
         }
 
         private bool BeUniqueEmail(string email)
         {
-            return this._userService.GetAllUsers().All(x => x.Email != email);
+            return this._userService.GetAllUsers().Where(u => u.Id != this._userId).All(x => x.Email != email);
         }
 
         private bool BeAValidPhoneNumber(string phone)
