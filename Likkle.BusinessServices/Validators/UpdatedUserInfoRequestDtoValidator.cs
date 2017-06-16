234eb9ec55a707af12807248fddd632a5bd9d45f
Kristian Azmanov <Kristian_Azmanov@epam.com>
2017-06-16 13:21:50 +0300

    Now when updating phone number it is allowed to enter empty value.

--- Likkle.BusinessServices/Validators/UpdatedUserInfoRequestDtoValidator.cs
+++ Likkle.BusinessServices/Validators/UpdatedUserInfoRequestDtoValidator.cs
@@ -36,7 +36,7 @@ namespace Likkle.BusinessServices.Validators
 
         private bool BeAValidPhoneNumber(string phone)
         {
-            return this._phoneValidationManager.PhoneNumberIsValid(phone);
+            return string.IsNullOrEmpty(phone) || this._phoneValidationManager.PhoneNumberIsValid(phone);
         }
     }
 }
