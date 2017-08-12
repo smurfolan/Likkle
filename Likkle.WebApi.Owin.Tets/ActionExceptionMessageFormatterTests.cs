54e9ff2e56545dc8b0fcdb5c0b512003e204d8a3
kristian.azmanov <kristian.azmanov@afusion.com>
2017-08-12 15:37:44 +0300

    Unit test added to test formatted exception response. Still few left to be implemented. Not trivial.

--- Likkle.WebApi.Owin.Tets/ActionExceptionMessageFormatterTests.cs
+++ Likkle.WebApi.Owin.Tets/ActionExceptionMessageFormatterTests.cs
@@ -1,10 +1,8 @@
 ﻿using Likkle.BusinessServices.Utils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
+using System.Web.Http.Controllers;
 
 namespace Likkle.WebApi.Owin.Tets
 {
@@ -19,7 +17,23 @@ namespace Likkle.WebApi.Owin.Tets
         [TestMethod]
         public void Exception_On_Get_Request_Is_Properly_formatted()
         {
-            throw new NotImplementedException();
+            // arrange
+            var action = new HttpActionContext(new HttpControllerContext()
+            {
+                Request = new HttpRequestMessage()
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri(@"http://localhost:54036/api/v1/areas/51BA94F2-CF55-E711-ABC5-001C42F4C661")
+                } 
+            }, new ReflectedHttpActionDescriptor());
+
+            // act
+            var result = ActionLevelExceptionManager.GetActionExceptionMessage(action);
+
+            // assert
+            Assert.AreEqual(@"Error while trying to perfrom GET request to /api/v1/areas/51BA94F2-CF55-E711-ABC5-001C42F4C661.", result.ErrorMessage);
+            Assert.AreEqual(@"Sorry for the inconvinience. Our team was notified so you can try again later.", result.KindMessage);
+            Assert.IsTrue(result.ErrorId != string.Empty);
         }
 
         [TestMethod]
