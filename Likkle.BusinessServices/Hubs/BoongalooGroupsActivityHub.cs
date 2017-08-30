8ab6a100b8adde921934f0a219da612f34f2cc80
Kristian Azmanov <kristian.azmanov@gmail.com>
2017-08-30 17:29:39 +0300

    Initial SignalR configuration.

---
+++ Likkle.BusinessServices/Hubs/BoongalooGroupsActivityHub.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace Likkle.BusinessServices.Hubs
+{
+    public class BoongalooGroupsActivityHub : Hub
+    {
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
+        }
+
+        #region Client RPCs
+        #endregion
+
+        #region Server-side RPCs
+        #endregion
+    }
+}
