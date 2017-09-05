c7382206a5f96669e470df11964ce0743cbf6a48
Kristian Azmanov <kristian.azmanov@gmail.com>
2017-09-05 17:47:13 +0300

    Added code to grab user id of the person who connected.

--- Likkle.BusinessServices/Hubs/BoongalooGroupsActivityHub.cs
+++ Likkle.BusinessServices/Hubs/BoongalooGroupsActivityHub.cs
@@ -5,17 +5,28 @@ namespace Likkle.BusinessServices.Hubs
 {
     public class BoongalooGroupsActivityHub : Hub
     {
+        private readonly string UserIdQueryStringParamName = "userId";
+        private readonly string CurrentlyConnectedUsersGroupName = "CurrentlyConnectedUsers";
         public override Task OnConnected()
         {
+            // It has to be explicitly passed by the client when creating the HubConnection. 
+            // In the .NET client it is as a second parameter - dictionary.
+            var userId = Context.QueryString[UserIdQueryStringParamName];
+            Groups.Add(userId, CurrentlyConnectedUsersGroupName);
+
             return base.OnConnected();
         }
 
         public override Task OnDisconnected(bool stopCalled)
         {
+            var userId = Context.QueryString[UserIdQueryStringParamName];
+            Groups.Remove(userId, CurrentlyConnectedUsersGroupName);
+
             return base.OnDisconnected(stopCalled);
         }
 
         #region Client RPCs
+
         #endregion
 
         #region Server-side RPCs
