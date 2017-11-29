57f1ce6b7c58bf9b4079d9cbc0cbe39ea2667b8e
Kristian Azmanov <kristian.azmanov@gmail.com>
2017-11-29 19:02:50 +0200

    [WIP] Added some unit tests. Need to see how to verify method call in Taks.Run.

---
+++ Likkle.WebApi.Owin.Tets/LikkleApiLoggerTests.cs
@@ -0,0 +1,48 @@
+﻿using Likkle.BusinessServices;
+using Likkle.WebApi.Owin.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+
+namespace Likkle.WebApi.Owin.Tets
+{
+    [TestClass]
+    public class LikkleApiLoggerTests
+    {
+        private readonly Mock<IConfigurationWrapper> _configurationWrapperMock;
+        private readonly Mock<IMailService> _mailServiceMock;
+
+        public LikkleApiLoggerTests()
+        {
+            this._configurationWrapperMock = new Mock<IConfigurationWrapper>();
+            this._mailServiceMock = new Mock<IMailService>();
+        }
+
+        [TestMethod]
+        public void We_Send_System_Exception_On_Email_If_Configured_In_Settings()
+        {
+            // arrange
+            this._configurationWrapperMock.Setup(cwm => cwm.MailSupportOnException).Returns(true);
+
+            // act
+            var logger = new LikkleApiLogger(this._configurationWrapperMock.Object, this._mailServiceMock.Object);
+            logger.OnActionException(
+                new HttpActionContext(
+                    new HttpControllerContext()
+                    {
+                        Request = new HttpRequestMessage()
+                        {
+                            Method = HttpMethod.Get,
+                            RequestUri = new System.Uri("https://www.google.bg/")
+                        }
+                    }, 
+                    new ReflectedHttpActionDescriptor() { }
+                ), 
+                new System.Exception());
+
+            // assert
+            this._mailServiceMock.Verify(msm => msm.SendEmailForThrownException(It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+        }
+    }
+}
