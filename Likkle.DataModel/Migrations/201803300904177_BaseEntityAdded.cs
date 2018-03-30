dc48f33061315a6930ec0e763653f90b229f4465
Kristian Azmanov <kristian.azmanov@gmail.com>
2018-03-30 14:51:14 +0300

    Removed reference to non existing column in a migration script

--- Likkle.DataModel/Migrations/201803300904177_BaseEntityAdded.cs
+++ Likkle.DataModel/Migrations/201803300904177_BaseEntityAdded.cs
@@ -10,12 +10,10 @@ namespace Likkle.DataModel.Migrations
             AddColumn("dbo.Areas", "DateCreated", c => c.DateTime(nullable: false));
             AddColumn("dbo.Groups", "DateCreated", c => c.DateTime(nullable: false));
             AddColumn("dbo.Users", "DateCreated", c => c.DateTime(nullable: false));
-            DropColumn("dbo.Groups", "VisibleToThePublic");
         }
         
         public override void Down()
         {
-            AddColumn("dbo.Groups", "VisibleToThePublic", c => c.Boolean(nullable: false));
             DropColumn("dbo.Users", "DateCreated");
             DropColumn("dbo.Groups", "DateCreated");
             DropColumn("dbo.Areas", "DateCreated");
